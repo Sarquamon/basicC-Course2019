a82916b82fbd29e1516105afd569c495c206bfa5
Salomon Piña <sarquamon@gmail.com>
2019-12-23 18:28:39 -0600

    Enlisted folders for course, ordered tasks, and added conditionals and operators

---
+++ 3. Operadores - Operators/Operadores-Operators.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    //Dentro de los operadores, c++ cuenta con los más basicos de la aritmetica.
+    // +, -, *, /
+    int valorA = 2;
+    int valorB = 2;
+
+    int suma = valorA + valorB;
+    int resta = valorA - valorB;
+    int multi = valorA * valorB;
+    int division = valorA / valorB;
+
+    cout << "La suma es: " << suma << endl;
+    cout << "La resta es: " << resta << endl;
+    cout << "La multi es: " << multi << endl;
+    cout << "La division es: " << division << endl;
+
+    system("pause");
+    return 0;
+}
---
+++ 4. Condicionales - Conditionals/Condicionales-Conditionals.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //Las condicionales funcionan para validar todo tipo de situaciones dentro
+    // de nuestro codigo
+
+    int valorA = 1;
+    int valorB = 2;
+
+    if (valorA > valorB){
+        cout << "El valor de a es mayor al valor de b" << endl;
+        cout << "El valor de a es: "<< valorA << ". El valor de b es: " << valorB << endl;
+    }else{
+        cout << "El valor de b es mayor al valor de a" << endl;
+        cout << "El valor de a es: "<< valorA << ". El valor de b es: " << valorB << endl;
+    }
+    
+
+    system("pause");
+    return 0;
+}
