ca5e8672529ac3afe701665aea6c831b2c80204e
Salomon Piña <sarquamon@gmail.com>
2019-12-23 19:00:30 -0600

    Made more explicit if-else declarations

--- 4. Condicionales - Conditionals/Condicionales-Conditionals.cpp
+++ 4. Condicionales - Conditionals/Condicionales-Conditionals.cpp
@@ -2,21 +2,29 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     //Las condicionales funcionan para validar todo tipo de situaciones dentro
     // de nuestro codigo
 
     int valorA = 1;
     int valorB = 2;
 
-    if (valorA > valorB){
+    if (valorA > valorB)
+    {
         cout << "El valor de a es mayor al valor de b" << endl;
-        cout << "El valor de a es: "<< valorA << ". El valor de b es: " << valorB << endl;
-    }else{
-        cout << "El valor de b es mayor al valor de a" << endl;
-        cout << "El valor de a es: "<< valorA << ". El valor de b es: " << valorB << endl;
+        cout << "El valor de a es: " << valorA << ". El valor de b es: " << valorB << endl;
     }
-    
+    else if (valorA == valorB)
+    {
+        cout << "El valor de a es igual al valor de b" << endl;
+        cout << "El valor de a es: " << valorA << ". El valor de b es: " << valorB << endl;
+    }
+    else
+    {
+        cout << "El valor de b es mayor al valor de a" << endl;
+        cout << "El valor de a es: " << valorA << ". El valor de b es: " << valorB << endl;
+    }
 
     system("pause");
     return 0;
